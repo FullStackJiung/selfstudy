69f978a511403902fe1b721c126d4566eb24157a
FullStackJiung <stacksjiung@gmail.com>
2023-09-18 12:09:38 +0900

    divide resulf double

--- baekjoon/c++problems/adivideb.cpp
+++ baekjoon/c++problems/adivideb.cpp
@@ -3,7 +3,11 @@ using namespace std;
 int main()
 {
     int a,b;
+    double c;
+
     cin >>a >>b;
-    cout << a/b;
+    c= a/(double)b;
+    cout.precision(10);
+    cout << c;
     return 0;
 }
