62617bdd116a97cc2b37c1337e5d6861967890be
FullStackJiung <144968195+FullStackJiung@users.noreply.github.com>
2023-09-19 13:34:45 -0400

    solve 11382

---
+++ baekjoon/c++problems/11382.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{//  A, B, C (1 ≤ A, B, C ≤ 10^12)
+    long a ;
+    long b ;
+    long c ;
+    cin >> a >> b >> c;
+    cout << a + b + c << endl;
+    return 0;
+}
