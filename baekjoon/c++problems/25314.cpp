13d51fe43403899bd4c5fb505ed8e403fd59d525
FullStackJiung <stacksjiung@gmail.com>
2023-10-01 10:13:10 +0900

    25317

---
+++ baekjoon/c++problems/25314.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string n = "long ";
+    string result;
+    int num = 0;
+
+    cin >> num;
+    for (int i = 0; i <num/4;i++){
+        result +=n;
+
+    }
+    cout << result <<"int";
+    return 0;
+}
