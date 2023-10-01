ec387175e333b318c0e5c43b93520ddbf9e02942
FullStackJiung <stacksjiung@gmail.com>
2023-09-30 13:22:17 -1200

    15552

---
+++ baekjoon/c++problems/15552.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // sync_with_stdio= false;
+    // cin.tie(null);
+    int size = 0;
+    int a = 0;
+    int b = 0;
+    cin >> size;
+ for(int i = 0; i < size; i++) {
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d\n", a + b);
+}
+
+    return 0;
+}
